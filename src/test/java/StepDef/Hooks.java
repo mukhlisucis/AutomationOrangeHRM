341db215e020d2f57c2774b0a2ded00bc7845186
Mukhlis Anshori Witanto <mukhlisanshori1997@gmail.com>
2023-03-21 18:38:30 +0700

    Template

---
+++ src/test/java/StepDef/Hooks.java
@@ -0,0 +1,28 @@
+package StepDef;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hooks {
+    public static WebDriver webDriver;
+
+    @Before
+    public void openBrowser(){
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+        webDriver  = new ChromeDriver();
+        String appUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+        webDriver.get(appUrl);
+        webDriver.manage().window().maximize();
+    }
+
+    @After
+    public void closeBrowser(){
+        webDriver.quit();
+    }
+
+
+
+}
